b1585fb571492e0a948b56be9a1167b2bbd05b4e
Leo Wong <leoelicos@gmail.com>
2022-05-31 01:15:59 +1000

    Define BlogTag model

---
+++ models/BlogTag.js
@@ -0,0 +1,45 @@
+/*
+ * Tech BlogTag
+ * models/Blog.js
+ * This script contains the necessary code to define the BlogTag entity
+ * Copyright 2022 Leo Wong
+ */
+
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class BlogTag extends Model {}
+
+BlogTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'blog',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog_tag',
+  }
+);
+
+module.exports = BlogTag;
