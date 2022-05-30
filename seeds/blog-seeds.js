2944376f22f442ec437c9e4e1f929c23f0df4c10
Leo Wong <leoelicos@gmail.com>
2022-05-31 01:18:58 +1000

    Seed blogs

--- seeds/blog-seeds.js
+++ seeds/blog-seeds.js
@@ -1,128 +1,126 @@
-[
+/*
+ * Tech Blog
+ * seeds/blog-seeds.js
+ * This script contains the seeds for the Blog entity
+ * Copyright 2022 Leo Wong
+ */
+
+const { Blog } = require('../models');
+
+const blogData = [
   {
-    date: '2022-26-05 00:00:00',
     title: 'Project 2, what to do?',
-    category: 'projects',
-    tags: ['Express.js', 'Handlebars.js'],
+    user_id: 1,
+    category_id: 3,
     summary: 'Tips and tricks to ace the project',
-    body: "The advice will come later, but for now here's a motivational quote: 'We cannot solve problems with the kind of thinking we employed when we came up with them.' — Albert Einstein",
+    text: "The advice will come later, but for now here's a motivational quote: 'We cannot solve problems with the kind of thinking we employed when we came up with them.' Albert Einstein",
   },
   {
-    date: '2022-24-05 00:00:00',
     title: 'Tech Blog, stuck in a bog?',
-    category: 'challenges',
-    tags: ['Express.js', 'Handlebars.js'],
+    user_id: 2,
+    category_id: 2,
     summary: '7 steps to creating the best tech blog in your class',
-    body: "The advice will come later, but for now here's a motivational quote: 'Learn as if you will live forever, live like you will die tomorrow.' — Mahatma Gandhi",
+    text: "The advice will come later, but for now here's a motivational quote: 'Learn as if you will live forever, live like you will die tomorrow.' Mahatma Gandhi",
   },
   {
-    date: '2022-05-17 00:00:00',
     title: 'E-commerce Back End, know your res.send!',
-    category: 'challenges',
-    tags: ['Express.js', 'Sequelize'],
+    user_id: 3,
+    category_id: 2,
     summary:
       "You'll see why ORMs are so useful, and why last week was so hard!",
-    body: '"Success is not final; failure is not fatal: It is the courage to continue that counts." — Winston S. Churchill',
+    text: "'The advice will come later, but for now here's a motivational quote: Success is not final; failure is not fatal: It is the courage to continue that counts.' Winston S. Churchill",
   },
   {
-    date: '2022-05-10 00:00:00',
     title: "Employee Management System, it's as big as it sounds!",
-    category: 'challenges',
-    tags: ['Express.js', 'SQL'],
+    user_id: 4,
+    category_id: 2,
     summary:
       'We break down the steps you need to knock this one out of the park!',
-    body: '"It is better to fail in originality than to succeed in imitation." — Herman Melville',
+    text: "The advice will come later, but for now here's a motivational quote: 'It is better to fail in originality than to succeed in imitation.' Herman Melville",
   },
   {
-    date: '2022-05-03 00:00:00',
     title: 'Note Taker, game changer!',
-    category: 'projects',
-    tags: ['Express.js', 'Heroku'],
+    user_id: 5,
+    category_id: 2,
     summary: 'Officially full stack! Welcome to the world of servers.',
-    body: '"The road to success and the road to failure are almost exactly the same." — Colin R. Davis',
+    text: "The advice will come later, but for now here's a motivational quote: 'The road to success and the road to failure are almost exactly the same.' Colin R. Davis",
   },
   {
-    date: '2022-04-26 00:00:00',
     title: 'Team Profile Generator, do it with Class!',
-    category: 'challenges',
-    tags: ['OOP'],
+    user_id: 6,
+    category_id: 2,
     summary:
       "OOPs! Did someone say Class? Here's how to constructor() your way to an A+",
-    body: "The advice will come later, but for now here's a motivational quote: 'Success usually comes to those who are too busy looking for it.' — Henry David Thoreau",
+    text: "The advice will come later, but for now here's a motivational quote: 'Success usually comes to those who are too busy looking for it.' Henry David Thoreau",
   },
   {
-    date: '2022-19-04 00:00:00',
     title: "Good README Generator, there's Node going back now!",
-    category: 'challenges',
-    tags: ['Node.js'],
+    user_id: 7,
+    category_id: 2,
     summary: "Everything you need to Node about... okay, okay, I'll stop now.",
-    body: "The advice will come later, but for now here's a motivational quote: 'Develop success from failures. Discouragement and failure are two of the surest stepping stones to success.' —Dale Carnegie'",
+    text: "The advice will come later, but for now here's a motivational quote: 'Develop success from failures. Discouragement and failure are two of the surest stepping stones to success.' Dale Carnegie'",
   },
   {
-    date: '2022-28-03 00:00:00',
-    title: 'Project 1, easier said than done!',
-    category: 'challenges',
-    tags: ['Third-Party APIs'],
-    summary:
-      "And you thought Weather Dashboard was hard! Here's how to make Project 1 work.",
-    body: "The advice will come later, but for now here's a motivational quote: Nothing in the world can take the place of Persistence. Talent will not; nothing is more common than unsuccessful men with talent. Genius will not; unrewarded genius is almost a proverb. Education will not; the world is full of educated derelicts. The slogan 'Press On' has solved and always will solve the problems of the human race.' —Calvin Coolidge",
-  },
-  {
-    date: '2022-04-07 00:00:00',
     title: 'Updated Portfolio, come again?',
-    category: 'homework',
-    tags: ['LinkedIn', 'GitHub'],
+    user_id: 8,
+    category_id: 1,
     summary:
       "Cross the T's and Dot the I's: here's what to cross and what to dot.",
-    body: "The advice will come later, but for now here's a motivational quote: 'There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind.' —Mister Rogers",
+    text: "The advice will come later, but for now here's a motivational quote: 'There are three ways to ultimate success: The first way is to be kind. The second way is to be kind. The third way is to be kind.' Mister Rogers",
   },
   {
-    date: '2022-24-03 00:00:00',
+    title: 'Project 1, easier said than done!',
+    user_id: 9,
+    category_id: 3,
+    summary:
+      "And you thought Weather Dashboard was hard! Here's how to make Project 1 work.",
+    text: "The advice will come later, but for now here's a motivational quote: 'The slogan 'Press On' has solved and always will solve the problems of the human race.' Calvin Coolidge",
+  },
+  {
     title: 'Weather Dashboard, .then what?',
-    category: 'homework',
-    tags: ['fetch'],
+    user_id: 10,
+    category_id: 1,
     summary: 'Everything you need to know about fetch, .then and catch',
-    body: "The advice will come later, but for now here's a motivational quote: 'Success is peace of mind, which is a direct result of self-satisfaction in knowing you made the effort to become the best of which you are capable.' —John Wooden",
+    text: "The advice will come later, but for now here's a motivational quote: 'Success is peace of mind, which is a direct result of self-satisfaction in knowing you made the effort to become the best of which you are capable.' John Wooden",
   },
   {
-    date: '2022-17-03 00:00:00',
     title: 'Day planner, delegate, delegate, delegate',
-    category: 'homework',
-    tags: ['Event Delegation'],
+    user_id: 11,
+    category_id: 1,
     summary: 'Let the third parties do the work!',
-    body: "The advice will come later, but for now here's a motivational quote: 'I never dreamed about success. I worked for it.' —Estée Lauder",
+    text: "The advice will come later, but for now here's a motivational quote: 'I never dreamed about success. I worked for it.' Estée Lauder",
   },
   {
-    date: '2022-10-03 00:00:00',
     title: "Code Quiz, wait, what's an object?",
-    category: 'homework',
-    tags: ['Local Storage'],
+    user_id: 12,
+    category_id: 1,
     summary: "Know your objects from your arrays, and you'll be fine!",
-    body: "The advice will come later, but for now here's a motivational quote: 'Success is getting what you want, happiness is wanting what you get.' ―W. P. Kinsella",
+    text: "The advice will come later, but for now here's a motivational quote: 'Success is getting what you want, happiness is wanting what you get.' W. P. Kinsella",
   },
   {
-    date: '2022-03-03 00:00:00',
     title: 'Password Generator, Math.what?',
-    category: 'homework',
-    tags: ['Javascript'],
+    user_id: 13,
+    category_id: 1,
     summary: "Generate? Math? Random? Here's how you do it.",
-    body: "The advice will come later, but for now here's a motivational quote: 'Concentrate all your thoughts upon the work in hand. The sun's rays do not burn until brought to a focus. ' — Alexander Graham Bell",
+    text: "The advice will come later, but for now here's a motivational quote: 'Concentrate all your thoughts upon the work in hand. The sun's rays do not burn until brought to a focus.' Alexander Graham Bell",
   },
   {
-    date: '2022-24-02 00:00:00',
     title: "Professional Portfolio, easy! Wait… what's a flex box?",
-    category: 'homework',
-    tags: ['CSS'],
+    user_id: 14,
+    category_id: 1,
     summary: "It's all about putting things in boxes.",
-    body: "The advice will come later, but for now here's a motivational quote: 'Either you run the day or the day runs you.' — Jim Rohn",
+    text: "The advice will come later, but for now here's a motivational quote: 'Either you run the day or the day runs you.' Jim Rohn",
   },
   {
-    date: '2022-02-17 00:00:00',
-    title: 'Code refactor, how do you even pronounce Horiseon?',
-    category: 'homework',
-    tags: ['HTML'],
+    title: "Code refactor, how do you even say 'Horiseon'?",
+    user_id: 15,
+    category_id: 1,
     summary:
       "Messy code is still good code, right? Here's why refactoring is super important.",
-    body: "The advice will come later, but for now here's a motivational quote: 'I'm a greater believer in luck, and I find the harder I work the more I have of it.' — Thomas Jefferson",
+    text: "The advice will come later, but for now here's a motivational quote: 'I'm a greater believer in luck, and I find the harder I work the more I have of it.' Thomas Jefferson",
   },
 ];
+
+const seedBlogs = () => Blog.bulkCreate(blogData);
+
+module.exports = seedBlogs;
