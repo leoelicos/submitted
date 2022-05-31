71ee6dadfff21d9920415594ebde4419a9bc3fe1
Leo Wong <leoelicos@gmail.com>
2022-05-31 11:06:40 +1000

    Add options for rendering date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,11 @@
 module.exports = {
   format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
+    const options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    };
+    return date.toLocaleDateString(undefined, options);
   },
 };
