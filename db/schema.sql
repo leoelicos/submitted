64cb17f0a839206a61ad40b881b200e23c8ccac3
Leo Wong <leoelicos@gmail.com>
2022-06-02 12:21:57 +1000

    Add comment header

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,9 @@
+/*
+ * Tech Blog
+ * schema.sql
+ * This script contains the necessary code to delete any DB that is existing and then create Tech Blog Database
+ * Copyright 2022 Leo Wong
+ */
+
 DROP DATABASE IF EXISTS techblog_db;
 CREATE DATABASE techblog_db;
