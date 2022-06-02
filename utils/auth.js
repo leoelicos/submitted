8e750bd90a3709d0fb9fa92af4a7d1266891d703
Leo Wong <leoelicos@gmail.com>
2022-06-02 12:23:29 +1000

    Add comment header, improve conditional syntax

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,17 @@
+/*
+ * Tech Blog
+ * utils/auth.js
+ * This script contains a custom middleware function which redirects a user to the login page if they are not logged in to the session
+ * Copyright 2022 Leo Wong
+ */
+
 const withAuth = (req, res, next) => {
   // if user is not logged in, redirect to login page, otherwise go ahead
   if (!req.session.logged_in) {
     res.redirect('/login');
-  } else {
-    next();
+    return;
   }
+  next();
 };
 
 module.exports = withAuth;
