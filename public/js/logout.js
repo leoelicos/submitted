34c21715ea066567a4aebc4a14af8a1f87f0f757
Leo Wong <leoelicos@gmail.com>
2022-06-02 12:23:18 +1000

    Add comment header

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,4 +1,9 @@
-/* From bcs */
+/*
+ * Tech Blog
+ * logout.js
+ * This script contains the necessary code to allow user to log out, or a new user to sign up
+ * Adapted from BCS resources 2022
+ */
 
 const logout = async () => {
   const response = await fetch('/api/user/logout', {
